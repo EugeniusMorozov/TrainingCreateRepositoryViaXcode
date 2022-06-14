d568a75a7d157adab5196bfd6c657b922107df00
Eugenius Morozov <emorozov777@gmail.com>
2022-06-14 12:27:23 +0300

    Initial Commit

---
+++ TrainingCreateRepositoryViaXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingCreateRepositoryViaXcode
+//
+//  Created by Evgeny Morozov on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
