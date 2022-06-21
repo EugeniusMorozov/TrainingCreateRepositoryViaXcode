d8b0e09c5b0ce6a4feba59d8b82867ff683e89e3
Eugenius Morozov <emorozov777@gmail.com>
2022-06-21 15:22:09 +0300

    Create FirstViewController.

--- TrainingCreateRepositoryViaXcode/FirstViewController.swift
+++ TrainingCreateRepositoryViaXcode/FirstViewController.swift
@@ -5,15 +5,12 @@
 //  Created by Evgeny Morozov on 21.06.2022.
 //
 
-import Foundation
 import UIKit
-
 final class FirstViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let subview = UIView()
         view.addSubview(subview)
+        view.backgroundColor = .systemYellow
     }
 }
